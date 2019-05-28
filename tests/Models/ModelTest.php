04e4a09d8af8900632d07996dc8f3ba0baf389ab
sealinesun <sealine0111@gmail.com>
2019-05-28 15:40:53 -0400

    Added offset method tests

--- tests/Models/ModelTest.php
+++ tests/Models/ModelTest.php
@@ -2,11 +2,41 @@
 
 namespace GuillermoandraeTest\Models;
 
-use Guillermoandrae\Models\AbstractModel;
 use PHPUnit\Framework\TestCase;
+use Guillermoandrae\Models\AbstractModel;
 
 class ModelTest extends TestCase
 {
+    public function testOffsetGet()
+    {
+        $data = ['hey' => 'there', 'us' => 'them'];
+        $model = $this->getMockForAbstractClass(AbstractModel::class, [$data]);
+        $this->assertSame('there', $model['hey']);
+    }
+
+    public function testOffsetSet()
+    {
+        $data = ['hey' => 'there'];
+        $model = $this->getMockForAbstractClass(AbstractModel::class, [$data]);
+        $model['you'] = 'two';
+        $this->assertSame('two', $model['you']);
+    }
+
+    public function testOffsetSetNullOffset()
+    {
+        $model = $this->getMockForAbstractClass(AbstractModel::class);
+        $model[] = 'two';
+        $this->assertSame('two', $model[0]);
+    }
+    
+    public function testOffsetUnset()
+    {
+        $data = ['hey' => 'there', 'me' => 'you'];
+        $model = $this->getMockForAbstractClass(AbstractModel::class, [$data]);
+        unset($model['me']);
+        $this->assertNull($model['me']);
+    }
+
     public function testToJson()
     {
         $model = $this->getMockForAbstractClass(AbstractModel::class);
