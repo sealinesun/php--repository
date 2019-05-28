dbb11bdc4f7ab066ecb29dcca292cba43f4f024d
sealinesun <sealine0111@gmail.com>
2019-05-28 15:21:59 -0400

    Good ole ArrayAccess

--- src/Models/ModelInterface.php
+++ src/Models/ModelInterface.php
@@ -5,6 +5,6 @@ namespace Guillermoandrae\Models;
 use Guillermoandrae\Common\JsonableInterface;
 use Guillermoandrae\Common\ArrayableInterface;
 
-interface ModelInterface extends ArrayableInterface, JsonableInterface
+interface ModelInterface extends \ArrayAccess, ArrayableInterface, JsonableInterface
 {
 }
