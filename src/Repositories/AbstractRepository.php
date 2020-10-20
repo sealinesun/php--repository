e3188a65e440da35ca6281fd5f0bb7e6b0cd3fd1
sealinesun <sealine0111@gmail.com>
2020-10-20 10:59:06 -0400

    Added docs for the constructor.

--- src/Repositories/AbstractRepository.php
+++ src/Repositories/AbstractRepository.php
@@ -3,9 +3,26 @@
 namespace Guillermoandrae\Repositories;
 
 use Guillermoandrae\Common\DumpableTrait;
-use Guillermoandrae\Models\ModelInterface;
 
 abstract class AbstractRepository implements RepositoryInterface
 {
     use DumpableTrait;
+
+    /**
+     * Repository options.
+     *
+     * @var mixed|null
+     */
+    protected $options;
+
+    /**
+     * Constructor. Provides the ability to inject dependencies using the RepositoryFactory.
+     *
+     * @see RepositoryFactory
+     * @param mixed|null $options  The repository options.
+     */
+    public function __construct($options = null)
+    {
+        $this->options = $options;
+    }
 }
